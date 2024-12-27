0d3a6330d53a5496c09a32b55a790ff9bcb3f81c
Francine Dalida <francine.dalida@gmail.com>
2024-12-27 19:12:47 +0800

    First Commit
    First Date: 12/27/24

---
+++ main.js
@@ -0,0 +1,71 @@
+
+// NOTE the "EventListener" when you scroll a shadow returns
+let header = document.querySelector('header');
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// clicking menu and making the navbar drop doown action
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// when you scroll the toggle will be gone
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+//AUTO SWIPER in HOME PART
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
+
+  //SWIPER in COMING SOON
+
+  var swiper = new Swiper(".coming-container", {
+    spaceBetween:20,
+    loop:true,
+    autoplay:{
+        delay: 55000,
+        disableOnInteraction:false,
+    },
+
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView:2,
+        },
+
+        568: {
+            slidesPerView:3,
+        },
+
+        768: {
+            slidesPerView:4,
+        },
+
+        968: {
+            slidesPerView:5,
+        },
+    },
+
+  });
